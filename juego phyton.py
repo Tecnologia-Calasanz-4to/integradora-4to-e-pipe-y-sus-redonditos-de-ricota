29ed23c2caf726c081e307d1cf2bee456c6c14d0
Gododoy <d.godoysilva@colegiocalasanz.edu.ar>
2026-06-22 10:48:53 -0300

    Add files via upload

---
+++ juego phyton.py
@@ -0,0 +1,6 @@
+def nombre_usuario(nombre):
+    return len(nombre)>=3 and nombre.isalpha
+def crea_codename(nombre, nivel):
+    return nombre[:4].upper()+ str(nivel)
+def vida_maxima(nivel):
+    return
