bfe215b2adb7697f0c430cb9530cfc88e152263a
pazgonzalezherra <p.gonzalezherra@gmail.com>
2026-06-22 11:40:31 -0300

    Add files via upload

---
+++ juego python.py
@@ -0,0 +1,13 @@
+def arma(poder):
+ if poder>= 80:
+        return "Legendaria"
+    elif poder>=40:
+    return "Media"
+else:
+    return "Debil"
+def es_critico(es_magica, nivel):
+    if arma>= es_magica or nivel>= 10
+    devolver="true"
+    return=devolver
+def dano_base(ataque, poder, defensa):
+    return (ataque+poder)= defensa
